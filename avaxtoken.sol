76eb2e3acb29c244f2f5e04ee5c782aa9ebd226f
Shaguftaseraj <ahmadshagufta229@gmail.com>
2024-07-04 17:28:22 +0600

    avaxtoken.sol

--- avaxtoken.sol
+++ avaxtoken.sol
@@ -6,12 +6,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DegenGamingToken is ERC20, Ownable {
     uint8 createDEC;
+    // Mapping to store the in-game store items and their costs in tokens
      mapping(string => uint256) private _itemPrices;
 
     event ItemRedeemed(address indexed player, string item);
 
     constructor(uint8 _dec) ERC20("DegenGamingToken", "DGT") Ownable(msg.sender){
         createDEC = _dec;
+        // Mint initial supply to the contract deployer (owner)
         _mint(msg.sender, 1000 * 10 ** uint8(_dec)); 
     }
 
@@ -23,7 +25,7 @@ contract DegenGamingToken is ERC20, Ownable {
         _mint(account, amount);
         emit Transfer(address(0), account, amount);
     }
-
+    // Function to redeem tokens for items in the in-game store
     function redeem(string memory item) public {
         require(_itemPrices[item] > 0, "Item not available for redemption");
         require(balanceOf(msg.sender) >= _itemPrices[item], "Insufficient balance");
@@ -33,9 +35,9 @@ contract DegenGamingToken is ERC20, Ownable {
 
         emit ItemRedeemed(msg.sender, item);
     }
-
+    // Function to burn tokens (anyone can do this)
     function burn(uint256 amount) public {
+         require(balanceOf(msg.sender) >= amount, "Insufficient balance");
         _burn(msg.sender, amount);
     }
 }
-
